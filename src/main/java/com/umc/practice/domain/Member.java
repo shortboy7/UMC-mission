ea7d5ba8dcb17e477ebd08539718887af866cd18
shortboy7 <thdwldnd7@naver.com>
2023-11-15 04:28:14 +0900

    feat/init project

---
+++ src/main/java/com/umc/practice/domain/Member.java
@@ -0,0 +1,50 @@
+package com.umc.practice.domain;
+
+import com.umc.practice.domain.enums.Gender;
+import com.umc.practice.domain.enums.MemberStatus;
+import com.umc.practice.domain.enums.SocialType;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String specAddress;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    private String email;
+
+    private Integer point;
+}
