aee00b60c56e0a8cf67656c78da80a1c2cf727ce
shortboy7 <thdwldnd7@naver.com>
2023-11-22 22:57:34 +0900

    Feat:ExceptionAdvice와 GeneralException 추가

--- src/main/java/com/umc/practice/global/ResponseType/exception/GeneralException.java
+++ src/main/java/com/umc/practice/global/ResponseType/exception/GeneralException.java
@@ -1,4 +1,16 @@
 package com.umc.practice.global.ResponseType.exception;
 
-public class GeneralException {
+import com.umc.practice.global.ResponseType.code.BaseErrorCode;
+import com.umc.practice.global.ResponseType.code.ErrorReasonDTO;
+
+public class GeneralException extends RuntimeException{
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
 }
